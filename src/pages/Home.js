7982b37db86497530ca8a2f96fe7bc6a58b5cbbe
William Giraldo <giraldinis@gmail.com>
2021-01-15 19:08:54 -0500

    added infowindow

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,9 @@ import {
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
-import printerimg from "../images/printer2.png";
+import greenPrinter from "../images/green_printer.png";
+import yellowPrinter from "../images/yellow_printer.png";
+import redPrinter from "../images/red_printer.png";
 import testUtils from "react-dom/test-utils";
 
 const libraries = ["places"];
@@ -23,6 +25,9 @@ const options = {
 };
 export default function Home() {
   const [printers, setPrinter] = useState([]);
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [markerMap, setMarkerMap] = useState({});
+  const [infoOpen, setInfoOpen] = useState(false);
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "",
     libraries,
@@ -43,23 +48,74 @@ export default function Home() {
   if (loadError) return "Error Loading Maps";
   if (!isLoaded) return "Loading Maps";
 
+  const markerLoadHandler = (marker, place) => {
+    return setMarkerMap((prevState) => {
+      return { ...prevState, [place.id]: marker };
+    });
+  };
+
+  const markerClickHandler = (event, place) => {
+    // Remember which place was clicked
+    setSelectedPlace(place);
+
+    // Required so clicking a 2nd marker works as expected
+    if (infoOpen) {
+      setInfoOpen(false);
+    }
+
+    setInfoOpen(true);
+
+    // If you want to zoom in a little on marker click
+    // if (zoom < 13) {
+    //   setZoom(13);
+    // }
+
+    // if you want to center the selected Marker
+    //setCenter(place.pos)
+  };
+
   return (
     <div>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
-        zoom={8}
+        zoom={17}
         center={center}
         options={options}
       >
-        {
-          printers.map(printer => {
-            console.log(printer.name)
-            return (
-              <Marker key={printer.name} position = {printer.coordinates} icon = {printerimg}/>
-            )
-          })
-        }
+        {printers.map((printer) => {
+          console.log(printer.name);
+          let printerimg = {};
+          if (printer.signal === "RED") {
+            printerimg = redPrinter;
+          } else if (printer.signal === "GREEN") {
+            printerimg = greenPrinter;
+          } else {
+            printerimg = yellowPrinter;
+          }
+
+          return (
+            <Marker
+              key={printer.name}
+              position={printer.coordinates}
+              icon={printerimg}
+              onLoad={(marker) => markerLoadHandler(marker, printer)}
+              onClick={(event) => markerClickHandler(event, printer)}
+            />
+          );
+        })}
+
+        {infoOpen && selectedPlace && (
+          <InfoWindow
+            anchor={markerMap[selectedPlace.id]}
+            onCloseClick={() => setInfoOpen(false)}
+          >
+            <div>
+              <h3>{selectedPlace.id}</h3>
+              <div>This is your info window content</div>
+            </div>
+          </InfoWindow>
+        )}
       </GoogleMap>
-    </div>  
+    </div>
   );
 }
